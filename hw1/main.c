544d8c15c737ad11819debc0f81ad25d9eae0e0f
Yunus Emre <yigityunus442@gmail.com>
2022-04-03 16:08:43 +0300

    added sibling to youngest child

--- hw1/main.c
+++ hw1/main.c
@@ -11,18 +11,24 @@ int main(){
     printf("grand parent pid: %d\n",getpid());
     for (i = 0; i < depth; i++){
         res = fork();
-        //printf("middle parent pid: %d parent pid %d\n", getpid(), getppid());
         if (res == 0){
         depth = depth - 1;
             while(depth > 0){
                 depth = depth - 1;
                 res = fork();
+
+                if(depth == 0 && res != 0)
+                    res = fork();
+                    
                 if(res != 0){
                     wait(NULL);
                     exit(0);
-                }        
+                }
+            
+
             printf("-----child pid: %d     parent pid: %d\n", getpid(), getppid());
             }
+
             pause();
         }
 
