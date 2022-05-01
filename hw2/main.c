967c89f155c49a3cf163bb5d2783165fd87fcb6d
Yunus Emre <yigityunus442@gmail.com>
2022-05-01 21:57:53 +0300

    final_version

--- hw2/main.c
+++ hw2/main.c
@@ -1,5 +1,3 @@
-//Rahman ve Rahim Olan Allah'ın adıyla
-
 /**
  * @file main.c
  * @author Yunus Emre Yiğit (yigity19@itu.edu.tr)
@@ -13,17 +11,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <semaphore.h>
+#include <semaphore.h> 
 #include <pthread.h>
 #include <stdbool.h>
 
+int nr = 1;
+
 sem_t full, empty, sema;
 
-
-const int nr = 1;
 int APPLICANT_NUM = 0;
-int registered_count = 0;
 
+/**
+ * @brief represents the applicants applying for jobs
+ * ni is the number of seconds interview takes for different jobs 
+ */
 struct applicant{
     int num;
     int ni;
@@ -31,17 +32,24 @@ struct applicant{
     bool registered;
 };
 
-struct applicant buffer[4];
-
+/**
+ * @brief Interviewer struct
+ *  has an id called num & a pointer to the array applicants struct in order to reach applicants
+ */
 struct interviewer{
     char num;
     struct applicant* applicant_ptr;
 };
 
-int* readFile(){
+/**
+ * @brief read input file
+ * 
+ * @param name file name to read
+ * @return return pointer to array of int whose values come from file 
+ */
+int* readFile(char name[]){
     FILE* fptr;
-    char ch;
-    fptr = fopen("input.txt","r");
+    fptr = fopen(name,"r");
     if(fptr == NULL){
         printf("file could not be opened");
         return NULL;
@@ -56,9 +64,11 @@ int* readFile(){
         i++;
     }
 
-    fptr = fopen("input.txt","r");
+    // return file pointer to the head of the input file 
+    // this is done in order to fill the array with numbers
+    fptr = fopen(name,"r"); 
     if(fptr == NULL){
-        printf("file could not be opened");
+        printf("file could not be opened!\n");
         return NULL;
     }
     const int deneme = counter;
@@ -74,89 +84,116 @@ int* readFile(){
     return array;
 }
 
+/**
+ * @brief Registers applicants. Acts like the Producer in producer-consumer problem
+ * 
+ * @param ptr pointer to the applicant list
+ * @return void* 
+ */
 void* registerApplicant(void* ptr){
-    //struct interviewer* interviewerPtr = ptr;
+    
+    // local pointer to the applicants list
     struct applicant* head = ptr;
 
     for(int i = 0; i < APPLICANT_NUM; i++){
-        //if(head[i].registered == false && head[i].interviewed == false){
-            printf("register empty\n");
-            sem_wait(&empty);
-            printf("register sema\n");  
-            sem_wait(&sema);
-            sleep(1);
-            head[i].registered = true;
-            buffer[i] = head[i];
-            buffer[i].registered = true; 
-            registered_count++;
-            printf("**Applicant %d's registration is done**\n", head[i].num);
-            sem_post(&sema);
-            sem_post(&full);
-            sem_post(&full);
-            sem_post(&full);
-            sem_post(&full);
-            
-        //}
+        // if waiting room is not empty, wait until it becomes empty !!!!!!!!
+        sem_wait(&empty); 
+        // if another is in critical section, wait until it finishtes its job
+        sem_wait(&sema);
+        // registering takes nr seconds, wait until nr seconds passes
+        sleep(nr);
+        // mark the current applicant as registered
+        head[i].registered = true;
+        // unlock the critical section semaphore
+        sem_post(&sema);
+        // increment full semaphore, as there are at least one applicant in the waiting room
+        sem_post(&full);
+        printf("Applicant %d's registration is done\n", head[i].num);
     }
     pthread_exit(NULL);
 }
 
+// interview counter used to count total number of interviews that are done so far
+int int_counter = 0;
+
+/**
+ * @brief Interview with an applicant whose is in the waiting room. Acts like a consumer  
+ * 
+ * @param ptr Pointer to the current interviewer (or consumer thread)
+ * @return void* 
+ */
 void* interviewApplicant(void* ptr){
+    // form a pointer to the calling interviewer
     struct interviewer* interviewerPtr = ptr;
+    // form a pointer to the start of the array of applicants list
     struct applicant* head = interviewerPtr->applicant_ptr;
 
+    
+    int counter = 0;
     for(int i = 0; i < APPLICANT_NUM; i++){
-            printf("interview full\n");
+           // wait until there is at least one applicant in the waiting room !!!!!!
             sem_wait(&full);
-            printf("interview sema\n");
+            // !!!!!!1
             sem_wait(&sema);
-          
-
-            for(int j = 0; j < APPLICANT_NUM; j++ ){
-                printf("+++ applicant %d situation %d, reg: %d\n",head[j].num, head[j].interviewed, head[j].registered);
-            }
-            
-            printf("Interviewer %d started interview with Applicant %d\n", interviewerPtr->num, head[i].num);
-            sleep(2);
-            head[i].interviewed = true;
-            
-            printf("Interviewer %d finished interview with Applicant %d\n", interviewerPtr->num, head[i].num);
-            
-            for(int j = 0; j < APPLICANT_NUM; j++ ){
-                printf("--- applicant %d situation %d, reg: %d\n",head[j].num, head[j].interviewed, head[j].registered);
+            counter=0;
+            // find index of the applicant which is registered and has not been interviewed 
+            for(int j = 0; j < APPLICANT_NUM; j++){
+                if(head[j].registered && !head[j].interviewed){
+                    break;
+                }
+                else
+                    counter++;
             }
 
-            printf("\n");
-            
+            // mark the found applicant as interviewed
+            head[counter].interviewed = true;
+
+            // increment semaphores
             sem_post(&sema);
             sem_post(&empty);
-        
+
+            // increment the total number of interviewed done so far
+            int_counter++;
+
+            printf("Interviewer %d started interview with Applicant %d\n", interviewerPtr->num, head[counter].num);
+            sleep(head[counter].ni);
+            printf("Interviewer %d finished interview with Applicant %d\n", interviewerPtr->num, head[counter].num);
+
+            // if interviews done so far exceeds or equal to the number of all applicants end interviewing
+            if(int_counter >= APPLICANT_NUM){
+                free(head);
+                printf("All applicants have interviewed successfully.\n");
+                break;
+            }
     }
     pthread_exit(NULL);
 
 }
 
-int main(){
+int main(int argc, char* argv[]){
 
-    int* array = readFile();
-
+    int* array = readFile(argv[1]);
+    nr = atoi(argv[2]);
     struct applicant applicants[APPLICANT_NUM];
     struct interviewer interviewers[3];
 
-    printf("apllicant count %d-------\n", APPLICANT_NUM);
+    // initializing semaphores
     sem_init(&empty, 0, APPLICANT_NUM);
     sem_init(&full,  0, 0);
     sem_init(&sema,  0, 1);
 
-
+    // mark all applicants as not registered and not interviewed
+    // copy all ni values to applicants list
+    // print all applicants as applied to receptionist
     for(int i = 0; i < APPLICANT_NUM; i++){
         applicants[i].num = i + 1;
+        printf("Applicant %d applied to the receptionist\n", applicants[i].num);
         applicants[i].ni = array[i];
         applicants[i].interviewed = false;
         applicants[i].registered = false;
     }
 
-    
+    // initialize interviewer structs    
     interviewers[0].applicant_ptr = applicants;
     interviewers[0].num = 1;
     interviewers[1].applicant_ptr = applicants;
@@ -164,10 +201,10 @@ int main(){
     interviewers[2].applicant_ptr = applicants;
     interviewers[2].num = 3;
 
-
+    // declare thread objects
     pthread_t recept, inter1, inter2, inter3;
 
-
+    // create threads
     if(pthread_create(&recept, NULL, registerApplicant, (void *) applicants)){
         printf("Could not create  thread");
         exit(-1);
@@ -188,12 +225,6 @@ int main(){
         exit(-1);
     }
     
-    //pthread_join(recept, NULL);
-    //pthread_join(inter1, NULL);
-    //pthread_join(inter2, NULL);
-    //pthread_join(inter3, NULL);
-
     pthread_exit(NULL);
-    //sem_destroy(&sema);
    return 0;
 }
